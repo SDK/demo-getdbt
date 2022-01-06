288b48134769b3d4e94f2799cafcfdfaf8bcde85
Sebastian Gonzalez <seba.cl@gmail.com>
2022-01-06 11:18:01 -0300

    added macro get columns

---
+++ models/staging/citibike_trips_macro_get_columns.sql
@@ -0,0 +1,13 @@
+
+{% set usertypes = dbt_utils.get_column_values(table=ref('citibike_trips_2018'), column='usertype') %}
+
+with
+{% for usertype in usertypes %}
+  table_{{usertype}} as (
+    select count(1) as count_{{usertype}} from {{ref('citibike_trips_2018')}} where usertype = '{{usertype}}'
+  ){% if not loop.last%} ,{% endif %}
+{% endfor %}
+
+select * from {% for usertype in usertypes %}  
+    table_{{usertype}}{% if not loop.last%} ,{% endif %}
+{% endfor %}
